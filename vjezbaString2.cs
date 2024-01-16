bb367832e2d2d9c676b17178333a7a802bc20130
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-01-16 12:26:49 +0100

    Add files via upload

---
+++ vjezbaString2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1;
+
+            Console.WriteLine("Upisi neku recenicu");
+            s1 = Console.ReadLine();
+
+            string s2 = s1.Replace(" ", "_");
+            Console.WriteLine(s2);
+            Console.ReadKey();
+           
+            
+        }
+    }
+}
